4f165a651e608f2b9f1e71dd9fe693e168f2deee
dpcob <dpcob@>
2022-09-11 11:39:12 +0900

    settings  installedapps 'blog'

--- dg/settings.py
+++ dg/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'django.contrib.sites',
-    'blog.apps.BlogConfig',
+    #'blog.apps.BlogConfig',
+    'blog'
 ]
 
 MIDDLEWARE = [
