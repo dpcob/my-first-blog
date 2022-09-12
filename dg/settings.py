a765a3292329bc44c26fb443fedde4df0210a083
dpcob <dpcob@>
2022-09-12 22:08:42 +0900

    installed_apps blog.apps.BlogConfig

--- dg/settings.py
+++ dg/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'blog',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
