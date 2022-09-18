ae551b4013b085b7eeebf379ec5c2167431ed20d
dpcob <dpcob@>
2022-09-19 07:52:19 +0900

    secret_key setting

--- dg/settings.py
+++ dg/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t#-(%*34t7vyi17@=o$$p+ufq(dq(*o%^tph*i*(mcyxs#8mm='
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
