bc49c3757a760fafa9a679ca50f35a6ca2d9970c
dpcob <dpcob@>
2022-10-02 08:42:44 +0900

    requirements と Debug=False

--- dg/settings.py
+++ dg/settings.py
@@ -27,7 +27,7 @@ except ImportError:
     pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
