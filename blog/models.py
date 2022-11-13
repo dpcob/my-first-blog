6c4c8b946b8e2d5738a605f20287b966b61828ac
dpcob <dpcob@>
2022-11-13 20:36:14 +0900

    no author

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    # author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
